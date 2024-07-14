19b52a2264741d636e1a697f25001e67be0395a0
Abu Hanif <hanifcse90@gmail.com>
2024-07-14 20:20:29 +0600

    Unhande Rejection add

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import mongoose from 'mongoose';
 
+import { Server } from 'http';
+
 import app from './app';
 
 import config from './app/config';
+
+let server: Server;
 
 async function main() {
   try {
@@ -15,3 +19,17 @@ async function main() {
   }
 }
 main();
+process.on('unhandledRejection', () => {
+  console.log(`😈 unahandledRejection is detected , shutting down ...`);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on('uncaughtException', () => {
+  console.log(`😈 uncaughtException is detected , shutting down ...`);
+  process.exit(1);
+});
